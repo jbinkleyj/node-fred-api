f4a02333bd9cee6fc06af89dc5062b9396093915
pastorsj <pastorsj@rose-hulman.edu>
2017-05-25 00:21:52 -0400

    Changed deploy script again

--- deploy-script.sh
+++ deploy-script.sh
@@ -6,6 +6,8 @@ TARGET_BRANCH="gh-pages"
 
 function doCompile {
   bundle exec middleman build --clean
+  mv build/* ./
+  rm -r build/
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
