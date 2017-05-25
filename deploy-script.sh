71b0d03183c929c4a1d37ca54d40b3e324952f46
pastorsj <pastorsj@rose-hulman.edu>
2017-05-25 01:08:08 -0400

    Perhaps I found the ssh key

--- deploy-script.sh
+++ deploy-script.sh
@@ -163,14 +163,13 @@ configure_repo() {
   git config user.name "Travis CI"
   git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-  ls ../
 
   ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
   ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
   ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
   ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
   openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out ../deploy_key -d
-  chmod 600 ../deploy_key
+  chmod 400 ../deploy_key
   eval `ssh-agent -s`
   ssh-add deploy_key
 }
